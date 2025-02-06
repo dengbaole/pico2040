f0435295b5b27637fafb0606bcc46afed3b16521
dengbaole <dengbaole155@gmail.com>
2025-02-06 16:22:32 +0800

    更新屏幕驱动

--- moudle/ssd1306_i2c.h
+++ moudle/ssd1306_i2c.h
@@ -4,7 +4,7 @@
 #include "platform.h"
 #include "bitmap.h"
 
-#define SSD1306_HEIGHT              32
+#define SSD1306_HEIGHT              64
 #define SSD1306_WIDTH               128
 
 #define SSD1306_I2C_ADDR            _u(0x3C)
